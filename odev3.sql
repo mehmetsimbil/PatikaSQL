cb95ecb072415841452ba4771b277d8acb18a0ce
Mehmet Şimbil <79629272+mehmetsimbil@users.noreply.github.com>
2022-06-20 14:10:04 +0300

    Create odev3.sql

---
+++ odev3.sql
@@ -0,0 +1,4 @@
+--SELECT country FROM country WHERE country LIKE 'A%a'
+--SELECT country FROM country WHERE LENGTH(country)>=6 AND country LIKE '%n'
+--SELECT title FROM film WHERE title ILIKE '%t%'
+--SELECT * FROM film WHERE title LIKE 'C%' AND LENGTH(title)>91 AND rental_rate=2.99; 
